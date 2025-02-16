c333eaae12de58a36f09a0486bfd59d12f41ba83
xuwei-k <6b656e6a69@gmail.com>
2025-02-16 20:01:18 +0900

    wip [skip ci]

--- scalafmt/src/main/scala/cheerpj_scala/Scalafmt.scala
+++ scalafmt/src/main/scala/cheerpj_scala/Scalafmt.scala
@@ -1,11 +1,26 @@
 package cheerpj_scala
 
+import org.scalafmt.config.FormatEvent
+import org.scalafmt.config.ScalafmtConfig
+import org.scalafmt.internal.BestFirstSearch
+import org.scalafmt.internal.FormatOps
+import org.scalafmt.internal.FormatWriter
+import scala.meta.inputs.Input
 import scala.scalajs.js.annotation._
 
 @JSExportTopLevel("Scalafmt")
 object Scalafmt {
   @JSExport
   def format(source: String): String = {
-    org.scalafmt.Scalafmt.format(source).get
+    val style = ScalafmtConfig.default
+    val runner = style.runner
+    val tree = runner.parse(Input.VirtualFile("<input>", source)).get
+    implicit val formatOps: FormatOps = new FormatOps(tree, style)
+    runner.event(FormatEvent.CreateFormatOps(formatOps))
+    implicit val formatWriter: FormatWriter = new FormatWriter(formatOps)
+    val res = BestFirstSearch(Set.empty)
+    val x = formatWriter.mkString(res.state)
+    println(s"formatted ${source == x}")
+    x
   }
 }
